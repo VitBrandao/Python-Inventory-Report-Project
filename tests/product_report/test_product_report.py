949539ca94203b08a8386a9b346e01093e174211
vitor <vitorsbsilva@gmail.com>
2022-08-12 14:56:57 -0300

    feat: (req.2) test_product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,26 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    novo_produto = Product(
+        12,
+        "Joystick",
+        "Multilaser",
+        "2022-08-12",
+        "2026-08-12",
+        "JS48",
+        "a temperatura ambiente"
+    )
+
+    texto_produto = novo_produto.__repr__()
+
+    # Quebrando string para não comprometer o Flake8
+    inicio = "O produto Joystick fabricado em 2022-08-12 por Multilaser "
+    meio = "com validade até 2026-08-12 precisa ser armazenado "
+    fim = "a temperatura ambiente."
+
+    texto_esperado = inicio + meio + fim
+
+    texto_produto_string = str(texto_produto)
+
+    assert texto_produto_string == texto_esperado
