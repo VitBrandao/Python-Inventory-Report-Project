754ce82341f710ece36f6ea3ec016ee1ec03c152
vitor <vitorsbsilva@gmail.com>
2022-08-12 14:33:19 -0300

    feat: (req.1) test_product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
-# req.1
+
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    novo_produto = Product(
+        12,
+        "Joystick",
+        "Multilaser",
+        "2022-08-12",
+        "2026-08-12",
+        "JS48",
+        "Manter a temperatura ambiente"
+    )
+
+    assert novo_produto.id == 12
+    assert novo_produto.nome_do_produto == "Joystick"
+    assert novo_produto.nome_da_empresa == "Multilaser"
+    assert novo_produto.data_de_fabricacao == "2022-08-12"
+    assert novo_produto.data_de_validade == "2026-08-12"
+    assert novo_produto.numero_de_serie == "JS48"
+
+    instrucao = "Manter a temperatura ambiente"
+    assert novo_produto.instrucoes_de_armazenamento == instrucao
