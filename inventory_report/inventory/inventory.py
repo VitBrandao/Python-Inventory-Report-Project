9f99de4b4aaba0a533eaa89f7ba23dc8b8478182
vitor <vitorsbsilva@gmail.com>
2022-08-16 12:02:53 -0300

    refactor: inventory.py (req.7)

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,24 +1,47 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory():
+    def import_csv(file_path):
+        inventory_list = []
+        with open(file_path, encoding="utf-8") as f:
+            file_reader = csv.DictReader(f, delimiter=",", quotechar='"')
+
+        for item in file_reader:
+            inventory_list.append(item)
+
+        return inventory_list
+
+    def import_json(file_path):
+        inventory_list = []
+        with open(file_path) as file:
+            read_file = file.read()
+            inventory_list = json.loads(read_file)
+
+        return inventory_list
+
+    def import_xml(file_path):
+        inventory_list = []
+        with open(file_path) as file:
+            read_file = xmltodict.parse(file.read())
+            inventory_list = read_file["dataset"]["record"]
+
+        return inventory_list
+
     @classmethod
     def import_data(self, file_path, string_type):
-        inventory_list = []
         if "csv" in file_path:
-            with open(file_path, encoding="utf-8") as f:
-                file_reader = csv.DictReader(f, delimiter=",", quotechar='"')
-
-                for item in file_reader:
-                    inventory_list.append(item)
+            inventory_list = self.import_csv(file_path)
 
         if "json" in file_path:
-            with open(file_path) as file:
-                content = file.read()
-                inventory_list = json.loads(content)
+            inventory_list = self.import_json(file_path)
+
+        if "xml" in file_path:
+            inventory_list = self.import_xml(file_path)
 
         if string_type == "simples":
             return SimpleReport.generate(inventory_list)
